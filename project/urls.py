02756cfa0b38a8fb514e20eee9447e03464d5954
abdelrahmanelsaeid <abdelrahmanelsaeid5@gmail.com>
2023-02-01 23:41:51 +0200

    static

--- project/urls.py
+++ project/urls.py
@@ -17,10 +17,12 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static 
+from personal.views import home
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('', home),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT) 
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT) 
